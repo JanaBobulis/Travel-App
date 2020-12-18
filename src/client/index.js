1146443f70d0b42e3d294599dc8b45e19dbb15eb
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2020-12-18 15:28:36 +0000

    import of functions and styles

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,21 @@
-// should import the main function of application JS, import scss, export main function from your applicatiion js. But in order to import, where will you need to export it?
 import { performAction } from './js/app'
+import { getWeatherbitData } from './js/app'
+import { getGeonamesData } from './js/app'
+import { getImageData } from './js/app'
+import { newElement } from './js/todolist'
+import { resetElement } from './js/todolist'
 
 import './styles/style.scss'
-
-
+import './styles/todo.scss'
+import './styles/media.scss'
 
 export { performAction }
-
-
-
+export { getGeonamesData }
+export { getWeatherbitData }
+export { getImageData }
+export { newElement }
+export { resetElement }
 
 document.getElementById('generate').addEventListener('click', performAction);
+document.getElementById('add-item').addEventListener('click', newElement);
+document.getElementById('clear').addEventListener('click', resetElement);
