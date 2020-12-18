080054355d6937a28cfd4a320fd10fa982edabf0
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2020-12-18 15:27:33 +0000

    Data decided between two files in the server folder

--- src/server/server.js
+++ src/server/server.js
@@ -1,41 +1,17 @@
-// Setup empty JS object to act as endpoint for all routes
-let projectData = {};
-// Require Express to run server and routes
 const express = require('express');
-// Start up an instance of app
 const app = express();
-
 const bodyParser = require('body-parser');
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+const cors = require('cors');
+const { response } = require('express');
+const route = require('./route');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('dist'));
+app.use(cors()); 
+app.use('/', route);
 
-// Cors for cross origin allowance
-const cors = require('cors');
-const { response } = require('express');
-app.use(cors()); //connecting project dependencies 
 
-// Setup Server
-const port = 4040; 
-const server = app.listen(port, listening);
-function listening(){
-   // console.log("server running");
-    console.log(`running on localhost: ${port}`);
-};
-
-// GET route
-app.get('/all', getData);
- function getData(req, res) {
-    console.log(req)
-    res.send(projectData);
-};
-
-//POST route with three pieces of data 
-app.post('/add', function (req, res){
-     newData = req.body;
-      projectData = newData;
-      console.log(projectData, "Server side post geonames");
-      res.send(projectData);
-});
+module.exports = app.listen(4040,() => {
+    console.log("App listening on port 4040")
+})
